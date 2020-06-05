0f458b15a908b9618534822db5ae35dcfa100c82
petra1 <petra@access-for-all.ch>
2020-06-05 19:36:40 +0200

    Ad a new fragment for the images to the project

--- app/src/main/java/ch/petraaccess_for_all/zfaaccessibilitydemo/MainActivity.java
+++ app/src/main/java/ch/petraaccess_for_all/zfaaccessibilitydemo/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder (
-                R.id.nav_home, R.id.contactFragment)
+                R.id.nav_home, R.id.contactFragment, R.id.imageFragment)
                 .setDrawerLayout (drawer)
                 .build ();
         NavController navController = Navigation.findNavController (this, R.id.nav_host_fragment);
