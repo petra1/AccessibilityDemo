93bf90a17f44e2158043046a5d5c0fde3854b05e
petra1 <petra@access-for-all.ch>
2020-09-12 14:44:42 +0200

    General cleanup and removed not used files.

--- app/src/main/java/ch/petraaccess_for_all/zfaaccessibilitydemo/MainActivity.java
+++ app/src/main/java/ch/petraaccess_for_all/zfaaccessibilitydemo/MainActivity.java
@@ -1,12 +1,6 @@
 package ch.petraaccess_for_all.zfaaccessibilitydemo;
 
 import android.os.Bundle;
-//import android.view.View;
-//import android.view.Menu;
-//import android.widget.EditText;
-
-//import com.google.android.material.floatingactionbutton.FloatingActionButton;
-//import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -40,14 +34,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController (this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController (navigationView, navController);
     }
-    /**
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater ().inflate (R.menu.main, menu);
-        return true;
-    }
-    **/
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController (this, R.id.nav_host_fragment);
