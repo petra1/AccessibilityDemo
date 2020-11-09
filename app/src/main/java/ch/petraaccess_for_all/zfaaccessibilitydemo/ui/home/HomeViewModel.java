a16cee5807781c489696cd8ecb4e7bdb45964744
petra1 <petra@access-for-all.ch>
2020-11-09 07:30:45 +0100

    Change contact information and made some change in the nav_header_main.xml

--- app/src/main/java/ch/petraaccess_for_all/zfaaccessibilitydemo/ui/home/HomeViewModel.java
+++ app/src/main/java/ch/petraaccess_for_all/zfaaccessibilitydemo/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<> ();
-        mText.setValue ("Welcome to Accessibility-Demo \n Foundation 'Access for all' " );
+        mText.setValue ("Welcome to Accessibility-Demo ");
     }
 
     public LiveData<String> getText() {
