67ce2f3ef82a143566a94955de501495a729d97b
petra1 <petra@access-for-all.ch>
2021-04-17 12:21:54 +0200

    Set the contentDescription for ImageButton2  in ImageButtonFragment.java

--- app/src/main/java/ch/petraaccess_for_all/accessibilitydemo/ImageButtonFragment.java
+++ app/src/main/java/ch/petraaccess_for_all/accessibilitydemo/ImageButtonFragment.java
@@ -44,6 +44,7 @@ public class ImageButtonFragment extends Fragment {
         View v = inflater.inflate (R.layout.fragment_image_button, container, false);
         ImageButton imageButton1 = (ImageButton) v.findViewById (R.id.imageButton);
         ImageButton imageButton2 = (ImageButton) v.findViewById (R.id.imageButton2);
+        imageButton2.setContentDescription(getString(R.string.get_the_answer_to_question_two));
         final TextView textView12 = (TextView) v.findViewById (R.id.textView12);
         final TextView textView13 = (TextView) v.findViewById (R.id.textView13);
 
